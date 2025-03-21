05fde0e6b9f0708b8a847d269eb2070bae814f1b
Dominic Navos <dominicnavos@gmail.com>
2025-03-21 17:53:25 +0800

    modified forgot password and reset password page

--- page/reset-password.php
+++ page/reset-password.php
@@ -20,14 +20,41 @@ $title = "Reset Password";
 ?>
 
 <?php ob_start(); ?>
-<link rel="stylesheet" href="../assets/css/login1.css">
+<link rel="stylesheet" href="../assets/css/login.css">
 <?php $styles = ob_get_clean(); ?>
 
 <?php ob_start(); ?>
-<div class="navbar-container">
-    <?php include "../shared/navbar_landing_page.php"; ?>
-</div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Rural Connect</a>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="aboutus.php">About Us</a></li>
+            <li class="nav-item"><a class="nav-link" href="value.php">Value</a></li>
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
 <form action="../logic/reset-password.php" method="post" id="loginform">
+    <div class="login-container">
+        <img src="../assets/img/misc/delivery_pic.jpeg" alt="Logo" />
+        <div class="login-form">
+            <h2>Reset Password</h2>
+            <input type="hidden" name="accountId" id="accountId">
+            <input type="hidden" name="token" id="token">
+            <input placeholder="Password" required aria-label="Password" type="password" id="password"
+                name="password" />
+            <input placeholder="Confirm Password" required aria-label="Confirm Password" type="password"
+                id="re-password" name="re-password" />
+            <button type="submit" name="submit">Submit</button>
+            <a class="create-account" href="../page/login.php">Back to Login →</a>
+        </div>
+    </div>
+</form>
+
+<!-- <form action="../logic/reset-password.php" method="post" id="loginform">
     <div class="login-container">
         <img src="../assets/img/misc/delivery_pic.jpeg" alt="Logo" />
         <div class="login-form">
@@ -44,7 +71,7 @@ $title = "Reset Password";
             <a class="create-account" href="../page/login.php">Back To Login →</a>
         </div>
     </div>
-</form>
+</form> -->
 <?php $content = ob_get_clean(); ?>
 
 <?php ob_start(); ?>
